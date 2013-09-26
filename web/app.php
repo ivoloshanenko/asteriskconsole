608c5965a20905bbea2d244d2ef05ac30041f575
Zuzle <zuzleadmin@gmail.com>
2013-09-26 23:48:01 +0400

    +

--- web/app.php
+++ web/app.php
@@ -5,10 +5,12 @@ require_once __DIR__.'/../src/init/init.php';
 use Symfony\Component\HttpFoundation\Request,
     Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Home page
+ */
 $app->get('/',
     function(Request $request) use ($app) {
-    	echo 'Hello !';
-        exit();
+        return $app['twig']->render('home', array());
     }
 )
 ->method('GET');
