40a68521760f77471f2b137fd9f73acefba35665
Zuzle <zuzleadmin@gmail.com>
2013-09-27 02:11:20 +0400

    browser template rendering

--- frontend/assets/app.js
+++ frontend/assets/app.js
@@ -1,105 +1,90 @@
 var app = {};
 
 app.initilize = function () {
-	app.$users_list = $('#users-list');
-	app.$create_form = $('#create-form');
-	app.list();
-	app.initialize_create();
+    app.initialize_templates();
+    app.$users_list = $('#users-list');
+    app.$create_form = $('#create-form');
+    app.list();
+    app.initialize_create();
+};
+
+app.initialize_templates = function () {
+    app.templates = {};
+    app.templates.form = twig({
+        id: "form",
+        href: "templates/form.twig",
+        async: false
+    });
+    app.templates.row = twig({
+        id: "row",
+        href: "templates/row.twig",
+        async: false
+    });
 };
 
 app.list = function () {
-	$.ajax({
-		url: 'list',
-		success: function(data) {
-			if (!data.success) return alert('error loading list');
-			_.each(data.list, function (user) {
-				app.initialize_row(user);
-			});
-		}
-	});
+    $.ajax({
+        url: 'list',
+        success: function(data) {
+            if (!data.success) return alert('error loading list');
+            _.each(data.list, function (user) {
+                app.initialize_row(user);
+            });
+        }
+    });
 };
 
 app.initialize_row = function (user) {
 
-	var $row,
-		$name,
-		$secret,
-		$callerid,
-		$context,
-		$pickupgroup,
-		$permit,
-		$nat,
-		$ipaddr,
-		$useragent,
-		$action,
-		$edit,
-		$remove;
+    var $row = $(twig({ ref: "row" }).render(user)),
+        $remove = $row.find('[data-role="remove"]'),
+        $edit = $row.find('[data-role="edit"]');
 
-	app.$users_list.append(
-		$row = $('<tr>')
-			.attr('id', user.id)
-			.append($name = $('<td>').text(user.name))
-			.append($secret = $('<td>').text(user.secret))
-			.append($callerid = $('<td>').text(user.callerid))
-			.append($context = $('<td>').text(user.context))
-			.append($pickupgroup = $('<td>').text(user.pickupgroup))
-			.append($permit = $('<td>').text(user.permit))
-			.append($nat = $('<td>').text(user.nat))
-			.append($ipaddr = $('<td>').text(user.ipaddr))
-			.append($useragent = $('<td>').text(user.useragent))
-			.append($action = $('<td>')
-				.append(
-					$edit = $('<button>').addClass('btn')
-					.append($('<i>').addClass('icon-edit')))
-				.append(
-					$remove = $('<button>').addClass('btn btn-danger')
-					.append($('<i>').addClass('icon-remove icon-white')))
-			)
-	);
+    app.$users_list.append($row);
 
-	$remove.click(function(){
-		$.ajax({
-			url: 'remove',
-			type: 'post',
-			success: function(data) {
-				if (!data.success) return alert('error removing');
-				$row.fadeOut(500);
-			}
-		});
-		return false;
-	});
+    $remove.click(function(){
+        $.ajax({
+            url: 'remove',
+            type: 'post',
+            success: function(data) {
+                if (!data.success) return alert('error removing');
+                $row.fadeOut(500);
+            }
+        });
+        return false;
+    });
 
 };
 
 app.initialize_create = function () {
 
-	app.$create_form.submit(function(){
-		$.ajax({
-			url: 'create',
-			type: 'post',
-			data: app.$create_form.serialize(),
-			success: function (data) {
-				if (data.errors) {
-					_.each(data.errors, function (error) {
-						alert(error.field + ': ' + error.text);
-					});
-					return;
-				}
-				if (!data.success) return alert('error creating');
+    app.$create_form.submit(function(){
+        $.ajax({
+            url: 'create',
+            type: 'post',
+            data: app.$create_form.serialize(),
+            success: function (data) {
+                if (data.errors) {
+                    _.each(data.errors, function (error) {
+                        alert(error.field + ': ' + error.text);
+                    });
+                    return;
+                }
+                if (!data.success) return alert('error creating');
 
-				app.initialize_row(data.user);
+                app.initialize_row(data.user);
 
-				$('#create').modal('hide');
-			}
-		});
+                $('#create').modal('hide');
+            }
+        });
 
-		return false;
-	});
+        return false;
+    });
 
 };
 
 $(document).ready(function(){
 
-	app.initilize();
+    app.initilize();
 
 });
