4feea0a729da4381d13eed6857fff378d95f9a38
Zuzle <zuzleadmin@gmail.com>
2013-09-26 23:37:49 +0400

    mysql connection + configs

--- src/init/init.php
+++ src/init/init.php
@@ -13,3 +13,21 @@ use Symfony\Component\HttpFoundation\Request,
 $app = new Silex\Application();
 $app['debug'] = true;
 
+/************************************************************
+ * Configs
+ */
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__."/../../config/config.yml", array(), new Igorw\Silex\ChainConfigDriver(array(new Instudies\Framework\Config\YamlCachableConfigDriver()))));
+
+/************************************************************
+ * Database connection
+ */
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'dbname' =>     $app['settings']['config']['db_connection']['dbname'],
+        'user' =>       $app['settings']['config']['db_connection']['user'],
+        'password' =>   $app['settings']['config']['db_connection']['password'],
+        'host' =>       $app['settings']['config']['db_connection']['host'],
+        'driver' =>     $app['settings']['config']['db_connection']['driver'],
+    ),
+));
+$app['db']->executeQuery("SET NAMES utf8;");
