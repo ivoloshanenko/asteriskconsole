b9d94c28e77dec3882ab8c31ed88f16a04ce271b
Zuzle <zuzleadmin@gmail.com>
2013-09-28 19:58:56 +0400

    creation and edition form unification

--- frontend/assets/app.js
+++ frontend/assets/app.js
@@ -1,9 +1,10 @@
 var app = {};
 
+app.modals = [];
+
 app.initilize = function () {
     app.initialize_templates();
     app.$users_list = $('#users-list');
-    app.$create_form = $('#create-form');
     app.list();
     app.initialize_create();
 };
@@ -35,11 +36,61 @@ app.list = function () {
     });
 };
 
+app.initialize_form = function (user, cb) {
+
+    var $modal = $(twig({ ref: "form" }).render({
+        form_id: user ? 'edit_user_' + user.id : 'create-form',
+        user: user
+    }));
+
+    _.each(app.modals, function ($modal, key) {
+        $modal.remove();
+        delete app.modals[key];
+    });
+
+    app.modals.push($modal);
+
+    var $form = $modal.find('form');
+
+    $('body').append($modal);
+
+    $form.submit(function(){
+        $.ajax({
+            url: 'post',
+            type: 'post',
+            data: $form.serialize(),
+            success: function (data) {
+                if (data.errors) {
+                    _.each(data.errors, function (error) {
+                        alert(error.field + ': ' + error.text);
+                    });
+                    return;
+                }
+                if (!data.success) return alert('error sending form');
+
+                cb(data);
+            }
+        });
+
+        return false;
+    });
+
+    return $modal;
+
+};
+
 app.initialize_row = function (user) {
 
     var $row = $(twig({ ref: "row" }).render(user)),
+        $callerid = $row.find('[data-role="callerid"]'),
         $remove = $row.find('[data-role="remove"]'),
         $edit = $row.find('[data-role="edit"]');
+
+    var callerid = $callerid.html().split('"');
+    if (callerid.length > 1) callerid = callerid[1];
+    else callerid = callerid[0];
+
+    $callerid.text(callerid);
 
     $remove.click(function(){
         if (confirm('Are you sure?')) {
@@ -56,42 +107,13 @@ app.initialize_row = function (user) {
     });
 
     $edit.click(function(){
-
-        var $edit_modal = $(twig({ ref: "form" }).render({
-            form_id: 'edit_user_' + user.id,
-            user: user
-        }));
-
-        var $edit_form = $edit_modal.find('form');
-
-        $('body').append($edit_modal);
-
-        $edit_form.submit(function(){
-            $.ajax({
-                url: 'post',
-                type: 'post',
-                data: $edit_form.serialize(),
-                success: function (data) {
-                    if (data.errors) {
-                        _.each(data.errors, function (error) {
-                            alert(error.field + ': ' + error.text);
-                        });
-                        return;
-                    }
-                    if (!data.success) return alert('error editing');
-
-                    var $new_row = app.initialize_row(data.user);
-                    $row.replaceWith($new_row);
-
-                    $edit_modal.modal('hide');
-                }
-            });
-
-            return false;
+        var $modal;
+        $modal = app.initialize_form(user, function (data) {
+            var $new_row = app.initialize_row(data.user);
+            $row.replaceWith($new_row);
+            $modal.modal('hide');
         });
-
-        $edit_modal.modal('show');
-
+        $modal.modal('show');
         return false;
     });
 
@@ -101,37 +123,16 @@ app.initialize_row = function (user) {
 
 app.initialize_create = function () {
 
-    app.$create = $(twig({ ref: "form" }).render({
-        form_id: 'create-form'
-    }));
+    var $create = $('[data-role="create"]');
 
-    app.$create.attr('id', 'create');
-
-    app.$create_form = app.$create.find('form');
-
-    $('body').append(app.$create);
-
-    app.$create_form.submit(function(){
-        $.ajax({
-            url: 'post',
-            type: 'post',
-            data: app.$create_form.serialize(),
-            success: function (data) {
-                if (data.errors) {
-                    _.each(data.errors, function (error) {
-                        alert(error.field + ': ' + error.text);
-                    });
-                    return;
-                }
-                if (!data.success) return alert('error creating');
-
-                var $row = app.initialize_row(data.user);
-                app.$users_list.prepend($row);
-
-                app.$create.modal('hide');
-            }
+    $create.click(function(){
+        var $modal;
+        $modal = app.initialize_form(null, function (data) {
+            var $row = app.initialize_row(data.user);
+            app.$users_list.prepend($row);
+            $modal.modal('hide');
         });
-
+        $modal.modal('show');
         return false;
     });
 
